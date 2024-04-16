1a5c48f753b31b60190e13025123d3297c1aefa7
vnikolai <nikolaychuk.volodymyr@gmail.com>
2024-04-16 10:25:27 +0300

    build using Qt6

--- workflows/build_linux.sh
+++ workflows/build_linux.sh
@@ -5,11 +5,17 @@ src=$(dirname $(realpath $0))/..
 build=$src/build
 echo $build
 
+rm -rdf $build
 mkdir $build
 cd $build
 pwd
 
-cmake $src -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON
+cmake $src -DCMAKE_BUILD_TYPE=Release \
+           -DPERFOMETER_BUILD_PRINTER=ON \
+           -DPERFOMETER_BUILD_SAMPLES=ON \
+           -DPERFOMETER_BUILD_VISUALIZER=ON \
+           -DPERFOMETER_BUILD_TESTS=ON \
+           -DPERFOMETER_BUILD_BENCHMARKS=ON
 make
 
 ctest --output-on-failure
