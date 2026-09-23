bfc9836fdf26add41b4eeced6ace1209ea4b3382
vnikolai <nikolaychuk.volodymyr@gmail.com>
2026-09-23 17:14:25 +0300

    test time format uses gtest

--- utils/test/test_format_time.cpp
+++ utils/test/test_format_time.cpp
@@ -21,55 +21,44 @@ SOFTWARE. */
 #include <utils/time.h>
 #include <iostream>
 
-template<typename T1, typename T2>
-void print_error(const T1& a, const T2& b, const char* desc_a, const char* desc_b)
-{
-    std::cout << "check failed " << desc_a << " != " << desc_b << std::endl;
-    std::cout << "Expected: " << b << ", actual: " << a << std::endl;
-}
+#include <gtest/gtest.h>
 
-#define CHECK(a, b) if (a != b) { print_error(a, b, #a, #b); result = -1; }
-
-int main(int argc, const char** argv)
+TEST(test_format_time, test)
 {
     using namespace perfometer::utils;
 
-    int result = 0;
+    EXPECT_EQ(time_to_string(3600), "1h");
+    EXPECT_EQ(time_to_string(5400), "1h 30m");
+    EXPECT_EQ(time_to_string(7200), "2h");
 
-    CHECK(time_to_string(3600), "1h");
-    CHECK(time_to_string(5400), "1h 30m");
-    CHECK(time_to_string(7200), "2h");
+    EXPECT_EQ(time_to_string(10000), "2h 46m 40s");
+    EXPECT_EQ(time_to_string(1000), "16m 40s");
+    EXPECT_EQ(time_to_string(100), "1m 40s");
+    EXPECT_EQ(time_to_string(10), "10s");
+    EXPECT_EQ(time_to_string(1), "1s");
+    EXPECT_EQ(time_to_string(0.1), "100ms");
+    EXPECT_EQ(time_to_string(0.5), "500ms");
+    EXPECT_EQ(time_to_string(0.01), "10ms");
+    EXPECT_EQ(time_to_string(0.001), "1ms");
+    EXPECT_EQ(time_to_string(0.0001), "100us");
+    EXPECT_EQ(time_to_string(0.00001), "10us");
+    EXPECT_EQ(time_to_string(0.000001), "1us");
+    EXPECT_EQ(time_to_string(0.0000001), "100ns");
+    EXPECT_EQ(time_to_string(0.00000001), "10ns");
+    EXPECT_EQ(time_to_string(0.000000001), "1ns");
+    EXPECT_EQ(time_to_string(0.0000000001), "0ns");
 
-    CHECK(time_to_string(10000), "2h 46m 40s");
-    CHECK(time_to_string(1000), "16m 40s");
-    CHECK(time_to_string(100), "1m 40s");
-    CHECK(time_to_string(10), "10s");
-    CHECK(time_to_string(1), "1s");
-    CHECK(time_to_string(0.1), "100ms");
-    CHECK(time_to_string(0.5), "500ms");
-    CHECK(time_to_string(0.01), "10ms");
-    CHECK(time_to_string(0.001), "1ms");
-    CHECK(time_to_string(0.0001), "100us");
-    CHECK(time_to_string(0.00001), "10us");
-    CHECK(time_to_string(0.000001), "1us");
-    CHECK(time_to_string(0.0000001), "100ns");
-    CHECK(time_to_string(0.00000001), "10ns");
-    CHECK(time_to_string(0.000000001), "1ns");
-    CHECK(time_to_string(0.0000000001), "0ns");
-
-    CHECK(time_to_string(1.4), "1.4s");
-    CHECK(time_to_string(11.4), "11.4s");
-    CHECK(time_to_string(11.47), "11.47s");
-    CHECK(time_to_string(111.47), "1m 51.47s");
-    CHECK(time_to_string(10.4), "10.4s");
-    CHECK(time_to_string(1.04), "1.04s");
-    CHECK(time_to_string(10.04), "10.04s");
-    CHECK(time_to_string(60), "1m");
-    CHECK(time_to_string(72), "1m 12s");
-    CHECK(time_to_string(3659), "1h 59s");
-    CHECK(time_to_string(3660), "1h 1m");
-    CHECK(time_to_string(3661), "1h 1m 1s");
-    CHECK(time_to_string(3601), "1h 1s");
-
-    return result;
+    EXPECT_EQ(time_to_string(1.4), "1.4s");
+    EXPECT_EQ(time_to_string(11.4), "11.4s");
+    EXPECT_EQ(time_to_string(11.47), "11.47s");
+    EXPECT_EQ(time_to_string(111.47), "1m 51.47s");
+    EXPECT_EQ(time_to_string(10.4), "10.4s");
+    EXPECT_EQ(time_to_string(1.04), "1.04s");
+    EXPECT_EQ(time_to_string(10.04), "10.04s");
+    EXPECT_EQ(time_to_string(60), "1m");
+    EXPECT_EQ(time_to_string(72), "1m 12s");
+    EXPECT_EQ(time_to_string(3659), "1h 59s");
+    EXPECT_EQ(time_to_string(3660), "1h 1m");
+    EXPECT_EQ(time_to_string(3661), "1h 1m 1s");
+    EXPECT_EQ(time_to_string(3601), "1h 1s");
 }
