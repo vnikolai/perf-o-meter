90d9cf3e9144e9c92f716056f68b7df1e1d9603a
vnikolai <nikolaychuk.volodymyr@gmail.com>
2026-09-25 00:13:23 +0300

    buildfix MSVC

--- cmake/benchmark.cmake
+++ cmake/benchmark.cmake
@@ -1,6 +1,6 @@
 # Prepare testing environment
 
-cmake_minimum_required(VERSION 3.14)
+cmake_minimum_required(VERSION 3.15)
 
 include (FetchContent)
 
@@ -8,5 +8,7 @@ FetchContent_Declare (googlebenchmark
                       GIT_REPOSITORY https://github.com/google/benchmark.git
                       GIT_TAG v1.9.5
 )
+
+set(BENCHMARK_ENABLE_TESTING OFF)
 
 FetchContent_MakeAvailable (googlebenchmark)
